49d28dc75337e27c8ec42741db1e0ed8705e1f21
Peiyao Zhao <38776849+yaoyaohuanghuang@users.noreply.github.com>
2024-10-18 17:27:50 +0800

    Update param_parser.py

--- param_parser.py
+++ param_parser.py
@@ -25,10 +25,7 @@ def parameter_parser():
                         help='training ratio during data split.')
     parser.add_argument('--test_ratio', type=float, default=0.1,
                         help='test ratio during data split.')
-    parser.add_argument('--seed', type=int, default=31, help='Random seed.')
     parser.add_argument('--set_seed', type=int, default=10, help='Random seed.') # 10
-    parser.add_argument('--samples', type=int, default=10000,  #
-                        help='samples per triplet loss.')
     parser.add_argument('--weight_decay', type=float, default=5e-4,
                         help='Weight decay (L2 loss on parameters).')
     parser.add_argument('--hidden', type=int, default=32,
@@ -39,14 +36,7 @@ def parameter_parser():
                         nargs="+",
                         type=str,
                         help="Methods to use.")
-    parser.add_argument('--loss_ratio', type=float, default=-1,
-                        help='the ratio of loss_pbnc to loss_pbrc. -1 means only loss_pbnc.')
     parser.set_defaults(all_methods=['spectral', 'DSGC'])
-    parser.add_argument("--feature_options",
-                        nargs="+",
-                        type=str,
-                        help="Features to use for SSSNET. Can choose from ['A_reg','L','given','None'].")
-    parser.set_defaults(feature_options=['A_reg'])
 
     # synthetic model hyperparameters below
     parser.add_argument("--seeds",
@@ -55,11 +45,6 @@ def parameter_parser():
                         help="seeds to generate random graphs.")
     parser.set_defaults(seeds=[10])  # [10, 20, 30, 40, 50]
 
-    parser.add_argument('--pre_epoch', type=int, default=30, help='epochs for the first stage in training.')
-    parser.add_argument('--pre_lr', type=float, default=0.01,  # default = 0.01
-                        help='Initial learning rate.')
-    parser.add_argument('--post_lr', type=float, default=0.01,  # default = 0.01
-                        help='Initial learning rate.')
     parser.add_argument('--epochs', type=int, default=300,
                         help='Number of maximum epochs to train.')
     parser.add_argument('--lr', type=float, default=0.01,  # default = 0.01
@@ -87,12 +72,6 @@ def parameter_parser():
                         help='Number of hops to consider for the random walk.')
     parser.add_argument('--tau', type=float, default=0.5,
                         help='the regularization parameter when adding self-loops to the positive part of adjacency matrix, i.e. A -> A + tau * I, where I is the identity matrix.')
-    parser.add_argument('--triplet_loss_ratio', type=float, default=0.1,
-                        help='Ratio of triplet loss to cross entropy loss in supervised loss part. Default 0.1.')
-    parser.add_argument('--link_sign_loss_ratio', type=float, default=0.1,
-                        help='Ratio of link sign loss to cut loss in self-supervised loss part.')
-    parser.add_argument('--supervised_loss_ratio', type=float, default=50,
-                        help='Ratio of factor of supervised loss part to self-supervised loss part.')
     parser.add_argument('--seed_ratio', type=float, default=0.1,
                         help='The ratio in the training set of each cluster to serve as seed nodes.')
     parser.add_argument('--size_ratio', type=float, default=1.5,
@@ -108,15 +87,7 @@ def parameter_parser():
     parser.add_argument('--regenerate_data', action='store_true', help='Whether to force creation of data splits.')
     parser.add_argument('--load_only', action='store_true', help='Whether not to store generated data.')
     parser.add_argument('--dense', action='store_true', help='Whether not to use torch sparse.')
-    parser.add_argument('-AllTrain', '-All', action='store_true',
-                        help='Whether to use all data to do gradient descent.')
-    parser.add_argument('--link_sign_loss', action='store_true', help='Whether to use add link sign loss.')
-    parser.add_argument('-SavePred', '-SP', action='store_true', help='Whether to save predicted labels.')
-    parser.add_argument('--no_self_supervised', action='store_true', help='Whether to remove self-supervised loss.')
-    parser.add_argument('--balance_theory', action='store_true', help='Whether to use social balance theory.')
-    parser.add_argument('--pbnc_nsvgd', action='store_true', help='Whether to use social balance theory.')
-    parser.add_argument('--pbnc_res', action='store_true', help='Whether to use social balance theory.')
-    parser.add_argument('--two_step_train', action='store_true', help='Whether to use social balance theory.')
+    
 
     parser.add_argument('--alpha', type=float, default=0,
                         help='Threshold in triplet loss for seeds.')
